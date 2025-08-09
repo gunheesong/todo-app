c914e801e960cdbcb1fa65a166c19a4931b0bb77
gunheesong <g4song@uwaterloo.ca>
2025-08-09 19:15:27 -0400

    Added form to allow additions to the todo list. Added the load function and actions for the server loading. I didnt know why actions wasnt getting called but apparently its a svelte framework thing. The server/database.js fns were in the tutorial. typescript not learned.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    let { data } = $props();
+
+</script>
+
+<div class="centered">
+    <h1>todos</h1>
+
+    <!--To add a create todo list, we add forms to the eqn-->
+    <form method="POST">
+        <label>add a todo:
+            <input 
+                type="text" 
+                name="description"
+                autocomplete="off"
+            />
+
+        </label>
+        <button>submit</button>
+    </form>
+
+    <ul class="todos">
+        {#each data.todos as todo (todo.id)}
+
+            <li>{todo.description}</li>
+        {/each}
+    </ul>
+
+</div>
+
+<style>
+    .centered {
+        max-width: 20em;
+        margin: 0 auto;
+    }
+</style>
